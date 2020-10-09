0fae111a1e0ba040876cc0a9b243fb4d6aa589ad
Iiro Nurmi <iinurmi@gmail.com>
2020-10-09 17:41:23 +0300

    cleaned initial commit

--- src/components/SideSectionComponent.js
+++ src/components/SideSectionComponent.js
@@ -8,8 +8,6 @@ const SideSectionComponent = ({picture}) => {
         <img src={picture} className="img-fluid" alt="profile_picture" />
         <div>
           Iiro Nurmi
-          24.08.1992, Helsinki
-          +358 456717792, iinurmi@gmail.com
           iinurmi.wordpress.com
           http://www.linkedin.com/pub/iiro-nurmi/65/15a/b77
 
